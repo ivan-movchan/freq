8c4c9a20b975bf57d13945ca4bc7e9803f150abf
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-03-25 17:01:18 +0300

    Code optimization, copyright notice update
    Thanks to HyperWin (https://github.com/HyperWinX) for his useful advices
    on code optimization.
    
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- freq.c
+++ freq.c
@@ -5,39 +5,36 @@
 #include <string.h>
 #include <limits.h>
 
-#define VERSION  "1.0"
+#define VERSION  "1.1"
 #define YEARS    "2025"
 #define AUTHOR   "Ivan Movchan <ivan.movchan.07@gmail.com>"
 #define HOMEPAGE "https://github.com/ivan-movchan/freq"
 
 #define MAX_CHAR UCHAR_MAX
 
-void main(int argc, char** argv)
+int main(int argc, char** argv)
 {
 	if (argc > 1)
 	{
 		if (!strcmp(argv[1], "-v"))
 		{
-			printf("freq %s\nCopyright (c) %s %s\n%s\n", VERSION, YEARS, AUTHOR, HOMEPAGE);
-		};
-	}
-	else
-	{
-		int counts[MAX_CHAR];
-	
-		for (int i = 0; i < MAX_CHAR; i++)
-		{
-			counts[i] = 0;
-		};
-
-		while (!feof(stdin))
-		{
-			counts[fgetc(stdin)]++;
-		};
-
-		for (int i = 0; i < MAX_CHAR; i++)
-		{
-			printf("%02X:  %d\n", i, counts[i]);
+			printf("freq %s\nCopyright (C) %s %s\n%s\n", VERSION, YEARS, AUTHOR, HOMEPAGE);
+			return EXIT_SUCCESS;
 		};
 	};
+
+	int counts[MAX_CHAR];
+	memset(counts, 0, (MAX_CHAR * sizeof(counts[0])));
+
+	while (!feof(stdin))
+	{
+		counts[fgetc(stdin)]++;
+	};
+
+	for (int i = 0; i < MAX_CHAR; ++i)
+	{
+		printf("%02X:  %d\n", i, counts[i]);
+	};
+
+	return EXIT_SUCCESS;
 }
