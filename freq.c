390e55a2598c94f6b800765a30a3a352e9bd7112
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-03-25 22:54:40 +0300

    Percentage frequency, final loop optimization
    freq no longer prints the number (and percentage frequency) of bytes that were
    not found in the file.
    
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

--- freq.c
+++ freq.c
@@ -23,17 +23,23 @@ int main(int argc, char** argv)
 		};
 	};
 
-	int counts[MAX_CHAR];
+	int counts[MAX_CHAR], len = 0;
 	memset(counts, 0, (MAX_CHAR * sizeof(counts[0])));
 
 	while (!feof(stdin))
 	{
 		counts[fgetc(stdin)]++;
+		len++;
 	};
+
+	len -= ((len > 1) ? 1 : 0);
 
 	for (int i = 0; i < MAX_CHAR; ++i)
 	{
-		printf("%02X:  %d\n", i, counts[i]);
+		if (counts[i] > 0)
+		{
+			printf("0x%02X:  %d (%.2f%%)\n", i, counts[i], (counts[i] * 100.0 / len));
+		};
 	};
 
 	return EXIT_SUCCESS;
