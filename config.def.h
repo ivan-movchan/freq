58145913ee7b0e7691357386b57fe71a274037a4
Ivan Movchan <ivan.movchan.07@gmail.com>
2025-04-09 19:38:21 +0300

    Separate configuration
    Signed-off-by: Ivan Movchan <ivan.movchan.07@gmail.com>

---
+++ config.def.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2025 Ivan Movchan <ivan.movchan.07@gmail.com>
+ *
+ * This file is a part of freq.
+ *
+ * freq is free software released under the terms of MIT License.
+ * See LICENSE file for further details.
+ */
+
+const int MAX_BYTE = 1024;
+
+const char* DISPLAY_FORMAT_BYTE       = "0x%02X";
+const char* DISPLAY_FORMAT_COUNT      = "\t%d";
+const char* DISPLAY_FORMAT_PERCENTAGE = "\t%.2f%%";
